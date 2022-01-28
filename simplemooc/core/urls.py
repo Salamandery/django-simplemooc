61f537074df21aaf8d4a3db2adaa404c99158118
salamandery <sp.ront@hotmal.com>
2022-01-28 07:20:51 -0300

    urls core, base template

--- simplemooc/core/urls.py
+++ simplemooc/core/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from simplemooc.core.views import contact, home
+from django.urls import path
 
 urlpatterns = [
-    path('', include('simplemooc.core.urls')),
-    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('contato/', contact, name="contact"),
 ]
